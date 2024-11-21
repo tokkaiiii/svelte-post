5447c5b044bd33a72ffbe3b001181de7668cd5cb
tokkaiiii <luv_3_u@naver.com>
2024-11-21 17:25:26 +0900

    :sparkles:feat(article-post): 게시물 상세 화면 추가
    - 게시물
      - 상세 화면 추가

---
+++ src/pages/ArticlePost.svelte
@@ -0,0 +1,10 @@
+<script>
+    import ArticlePostDetail from "../components/ArticlePostDetail.svelte";
+    import ArticleHeader from "../components/ArticleHeader.svelte";
+    import ArticleNavigation from "../components/ArticleNavigation.svelte";
+    import Footer from "../components/Footer.svelte";
+</script>
+<ArticleHeader/>
+<ArticleNavigation/>
+<ArticlePostDetail/>
+<Footer/>
--- src/router.svelte
+++ src/router.svelte
@@ -4,9 +4,11 @@
     import Login from "./pages/Login.svelte";
     import Register from "./pages/Register.svelte";
     import Post from "./pages/Articles.svelte";
+    import ArticlePost from "./pages/ArticlePost.svelte";
 </script>
 <Route path="/" redirect="/home"/>
 <Route path="/home/*"><Home/></Route>
 <Route path="/login"><Login/></Route>
 <Route path="/register"><Register/></Route>
 <Route path="/articles"><Post/></Route>
+<Route path="/articles/post"><ArticlePost/></Route>
